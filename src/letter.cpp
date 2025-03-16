80e9a3f79f151e534c3943210eb37e26e25910ed
NexusAdept <jones-dow@list.ru>
2025-03-16 15:57:24 +0300

    Repository cleaning
    - deleted unnecessary temporary files
    - deleted unnecessary files from old version of project
    - renamed and moved directories

---
+++ src/letter.cpp
@@ -0,0 +1,94 @@
+#include "letter.h"
+
+Letter::Letter()
+{
+	origin = "";
+	technic = "";
+	printable = "";
+    isConsonant = false;
+    isVolve = false;
+    syll = 0;
+    pwr = 0;
+    number = 0;
+    word = 0;
+    pEnd = 0;
+    fw_pos = 0;
+    accent = false;
+}
+
+Letter::Letter(std::string symbol)
+{
+	origin = symbol;
+	technic = "+";
+	printable = symbol;
+	isConsonant = false;
+	isVolve = false;
+	syll = 0;
+	pwr = 0;
+	number = 0;
+	word = 0;
+	pEnd = 0;
+    fw_pos = 0;
+	accent = false;
+}
+
+Letter::~Letter()
+{
+}
+
+std::string Letter::getLetter()
+{
+	std::string outLetter = "";
+	outLetter += origin;
+	outLetter += "(";
+	outLetter += technic;
+	outLetter += printable;
+	outLetter += ")";
+	return outLetter;
+}
+
+std::string Letter::getLetterRepr()
+{
+	std::string outLetter = "";
+	outLetter += origin;
+	outLetter += "'";
+	outLetter += technic;
+	outLetter += "'";
+	outLetter += printable;
+	outLetter += "'";
+	outLetter += (isConsonant ? "C" : "-");
+	outLetter += (isVolve ? "V" : "-");
+	outLetter += std::to_string(number);
+	outLetter += "'";
+	outLetter += std::to_string(syll);
+	outLetter += "'";
+	outLetter += std::to_string(word);
+	return outLetter;
+}
+
+bool Letter::operator==(const Letter& letter) const
+{
+	if (this->origin != letter.origin)
+		return false;
+	if (this->technic != letter.technic)
+		return false;
+	if (this->printable != letter.printable)
+		return false;
+	if (this->isConsonant != letter.isConsonant)
+		return false;
+	if (this->isVolve != letter.isVolve)
+		return false;
+	if (this->syll != letter.syll)
+		return false;
+	if (this->pwr != letter.pwr)
+		return false;
+	if (this->number != letter.number)
+		return false;
+	if (this->word != letter.word)
+		return false;
+	if (this->pEnd != letter.pEnd)
+		return false;
+	if (this->accent != letter.accent)
+		return false;
+	return true;
+}

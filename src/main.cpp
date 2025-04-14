1d20b1f8fe6e983b972cea39b9b00900aca6e5a5
r0shn1ch <jones-dow@list.ru>
2025-04-14 12:42:30 +0300

    main.cpp Qt implementation
    app reads file with Qt functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,50 +1,49 @@
 #include "qtmainwindow.h"
 #include <QApplication>
-#include <fstream>
-#include <chrono>
+#include <QFile>
+#include <QTextStream>
+#include <QDebug>
+#include <QElapsedTimer>
+#include <QTextCodec>
 
 #include "phonotext.h"
 #include "proccessing.h"
 
 int main(int argc, char *argv[])
 {
-    auto begin = std::chrono::steady_clock::now();
+    QElapsedTimer timer;
+    timer.start();
 
 #ifdef _WIN32
-    system("chcp 65001");
+    QTextCodec *codec = QTextCodec::codecForName("UTF-8");
+    QTextCodec::setCodecForLocale(codec);
 #endif
 
-    std::ifstream fin;
-    fin.open("res/data/in1.txt", std::ios_base::in);
-    if (!fin.is_open()){
-        std::cout << "File cannot be opened" << std::endl;
+    QFile file("res/data/in1.txt");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+        qDebug() << "File cannot be opened";
         return 0;
-    };
-
-    std::cout << "start\n";
-
-    std::string data;
-
-    while (!fin.eof())
-    {
-        std::string line;
-        std::getline(fin, line);
-        data += line + '\n';
     }
 
-    fin.close();
-    std::cout << "Read End";
-    Phonotext pt(data);
+    qDebug() << "start";
+
+    QString data;
+    QTextStream in(&file);
+    in.setCodec("UTF-8"); // кодировка UTF-8
+    data = in.readAll();
+
+    file.close();
+    qDebug() << "Read End";
+
+    Phonotext pt(data.toStdString());
     Proccessing proc(pt, "rus", 0., 100.);
 
     proc.createJson("res/data/outJson.json");
     proc.print("res/data/out.txt");
 
-    auto end = std::chrono::steady_clock::now();
-    auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
-    std::cout << "The time: " << elapsed_ms.count() << " ms\n";
+    qint64 elapsed_ms = timer.elapsed();
+    qDebug() << "The time:" << elapsed_ms << "ms";
 
-    //frontend
     QApplication a(argc, argv);
     QtMainWindow w;
     w.show();
